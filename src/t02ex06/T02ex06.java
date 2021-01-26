e54bbcfd124be38932d484612f4b1c3adc740ea5
Rodolfo Lazo <rodolfo.lazo@outlook.com>
2021-01-26 13:54:18 +0000

    Commit inicial

---
+++ src/t02ex06/T02ex06.java
@@ -0,0 +1,33 @@
+/*
+ * and open the template in the editor.
+ */
+package t02ex06;
+
+/**
+ * @author rodo
+ */
+public class T02ex06 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // Podemos utilizar indistintamente caracteres o su equivalente en ascii
+        
+        char character1 = 72;  
+        char character2 = 'e';
+        char character3 = 'l';
+        char character4 = 'l';
+        char character5 = 'o';
+        
+        System.out.print(character1);
+        System.out.print(character2);
+        System.out.print(character3);
+        System.out.print(character4);
+        System.out.println(character5);
+        
+        
+    }
+    
+}
